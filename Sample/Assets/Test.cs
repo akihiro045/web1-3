66b2843f3010a42013cf53bcc1cec8c89b3f2afe
Unknown <g1727045@st.t-kougei.ac.jp>
2018-05-07 17:32:19 +0900

    最初のコミット

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector2 stratPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - stratPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+    }
+}
